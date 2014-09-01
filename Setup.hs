6946c2868129c321f3266423118b13709c8f2182
Marios Titas <redneb@gmx.com>
2014-09-01 03:43:00 +0100

    remove System.Linux.Btrfs.ByteString from the cabal file

--- Setup.hs
+++ Setup.hs
@@ -2,14 +2,30 @@ import Distribution.Simple
 import Distribution.Simple.Setup
 import Distribution.Simple.Utils
 import Distribution.Simple.BuildPaths
+import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription
+import Distribution.ModuleName (fromString)
 import System.IO
 import System.FilePath
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {buildHook = customBuildHook}
+main = defaultMainWithHooks simpleUserHooks
+    { confHook = customConf
+    , buildHook = customBuild
+    }
 
-customBuildHook desc lbi hooks flags = do
-    let verbosity = fromFlag (buildVerbosity flags)
+customConf gh cFlags = do
+    lbi <- confHook simpleUserHooks gh cFlags
+    -- modify the package description to add the auto-generated module
+    let desc = localPkgDescr lbi
+        Just lib = library desc
+        modName = fromString "System.Linux.Btrfs.ByteString"
+        lib' = lib {exposedModules = exposedModules lib ++ [modName]}
+        desc' = desc {library = Just lib'}
+    return lbi {localPkgDescr = desc'}
+
+customBuild desc lbi hooks bFlags = do
+    let verbosity = fromFlag (buildVerbosity bFlags)
 
     -- create System/Linux/Btrfs/ByteString.hsc
     let dir = autogenModulesDir lbi </> "System/Linux/Btrfs"
@@ -21,4 +37,4 @@ customBuildHook desc lbi hooks flags = do
             contents <- hGetContents iHdl
             hPutStr oHdl contents
 
-    buildHook simpleUserHooks desc lbi hooks flags
+    buildHook simpleUserHooks desc lbi hooks bFlags
