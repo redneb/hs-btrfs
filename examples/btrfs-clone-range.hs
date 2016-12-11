8dc027e733ac4ca0f44a819f4b3b55ea6fe71bbe
Marios Titas <redneb@gmx.com>
2016-12-11 13:50:06 +0200

    examples/btrfs-clone-range.hs: print error msg when given invalid cli args

--- examples/btrfs-clone-range.hs
+++ examples/btrfs-clone-range.hs
@@ -1,9 +1,23 @@
-import System.Environment
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitFailure)
+import System.IO (stderr, hPutStrLn)
+import Text.Read (readMaybe)
 
 import System.Linux.Btrfs
 
 main :: IO ()
 main = do
-    [srcPath, srcOff, srcLen, dstPath, dstOff] <- getArgs
-    cloneRange srcPath (read srcOff) (read srcLen)
-               dstPath (read dstOff)
+    args <- getArgs
+    case args of
+        [srcPath, srcOffS, srcLenS, dstPath, dstOffS]
+            | Just srcOff <- readMaybe srcOffS
+            , Just srcLen <- readMaybe srcLenS
+            , Just dstOff <- readMaybe dstOffS ->
+                cloneRange srcPath srcOff srcLen dstPath dstOff
+        _ -> do
+            prog <- getProgName
+            hPutStrLn stderr "Invalid command line arguments"
+            hPutStrLn stderr $
+                "Usage: " ++ prog ++
+                " SOURCE SOURCE_OFF SOURCE_LEN DEST DEST_OFF"
+            exitFailure
